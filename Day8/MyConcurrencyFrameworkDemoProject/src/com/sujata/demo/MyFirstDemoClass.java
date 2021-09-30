dd704b9e4c621b936e17ac5a494b88cb77674fb2
Sujatabatra <sujatabatra@hotmail.com>
2021-09-30 12:40:33 +0530

    Add files via upload

---
+++ Day8/MyConcurrencyFrameworkDemoProject/src/com/sujata/demo/MyFirstDemoClass.java
@@ -0,0 +1,37 @@
+package com.sujata.demo;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+class ThreadJob implements Runnable{
+
+	private int counter;
+	
+	public ThreadJob(int counter) {
+		super();
+		this.counter = counter;
+	}
+
+	@Override
+	public void run() {
+		System.out.println(Thread.currentThread().getName()+" counter : "+counter);
+		
+	}
+	
+}
+public class MyFirstDemoClass {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * Factory Design Pattern
+		 */
+		Executor threadPool=Executors.newFixedThreadPool(4);
+		
+		for(int jobId=1;jobId<=10;jobId++) {
+			threadPool.execute(new ThreadJob(jobId));
+		}
+		
+	}
+
+}
