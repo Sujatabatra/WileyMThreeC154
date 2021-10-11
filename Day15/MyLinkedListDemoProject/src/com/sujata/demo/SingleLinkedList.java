0f03227e5a578b73e378a54670ee2af3afccc8e8
Sujatabatra <sujatabatra@hotmail.com>
2021-10-11 12:38:15 +0530

    Add files via upload

--- Day15/MyLinkedListDemoProject/src/com/sujata/demo/SingleLinkedList.java
+++ Day15/MyLinkedListDemoProject/src/com/sujata/demo/SingleLinkedList.java
@@ -14,32 +14,56 @@ public class SingleLinkedList {
 		}
 
 	}
-	
+
 	Node head;
-	
+
 	public void insert(int data) {
-		Node newNode=new Node(data, null);
-		//if list is not there, make newly created node as fisrt node by pointing head on newly created node
-		if(head==null)
-			head=newNode;
+		Node newNode = new Node(data, null);
+		// if list is not there, make newly created node as fisrt node by pointing head
+		// on newly created node
+		if (head == null)
+			head = newNode;
 		else {
-			//if list is there , move head to end of the list
-			Node lastNode=head;
-			while(lastNode.next!=null)
-				lastNode=lastNode.next;
-			
-			//Already we reached to the last node of the list, now attach newly created node to the last
-			lastNode.next=newNode;
+			// if list is there , move head to end of the list
+			Node lastNode = head;
+			while (lastNode.next != null)
+				lastNode = lastNode.next;
+
+			// Already we reached to the last node of the list, now attach newly created
+			// node to the last
+			lastNode.next = newNode;
 		}
-		
-		
+
 	}
-	
+
 	public void traverse() {
-		Node iterator=head;
-		while(iterator!=null) {
+		Node iterator = head;
+		while (iterator != null) {
 			System.out.println(iterator.data);
-			iterator=iterator.next;
+			iterator = iterator.next;
+		}
+	}
+
+	public void delete(int data) {
+		// if list is there
+		if (head != null) {
+			// if Node which is supposed to be deleted is first Node
+			if (head.data == data) {
+				head = head.next;
+			} 
+			else {
+				Node previousNode = head;
+				// pointing previousNode to one node before the delete Node or either data don't
+				// exist
+				while ( previousNode.next != null && previousNode.next.data != data ) {
+					previousNode = previousNode.next;
+				}
+				// if the data found which we want to delete
+				if (previousNode.next != null) {
+					previousNode.next = previousNode.next.next;
+				}
+
+			}
 		}
 	}
 }
--- Day15/MyLinkedListDemoProject/src/com/sujata/demo/SinglyLinkedListPresentation.java
+++ Day15/MyLinkedListDemoProject/src/com/sujata/demo/SinglyLinkedListPresentation.java
@@ -8,7 +8,8 @@ public class SinglyLinkedListPresentation {
 	public void showMenu() {
 		System.out.println("1. Add element to Linked List");
 		System.out.println("2. Traverse Linked List");
-		System.out.println("3. Exit");
+		System.out.println("3. Deleting data fron  List");
+		System.out.println("4. Exit");
 	}
 	
 	public void performMenu(int choice) {
@@ -24,6 +25,11 @@ public class SinglyLinkedListPresentation {
 			singleLinkedList.traverse();
 			break;
 		case 3:
+			System.out.println("Enter Element to be deleted : ");
+			int elemt=scanner.nextInt();
+			singleLinkedList.delete(elemt);
+			break;
+		case 4:
 			System.out.println("Thanks for Using out System");
 			System.exit(0);
 		default:
