43e4b935c14c472fa65f2c2b38cd93462673344f
Sujatabatra <sujatabatra@hotmail.com>
2021-10-19 14:55:36 +0530

    Add files via upload

---
+++ Day20/MyPerformanceSpELProject/src/com/sujata/demo/Circle.java
@@ -0,0 +1,16 @@
+package com.sujata.demo;
+
+public class Circle {
+
+	private double pi;
+
+	public void setPi(double pi) {
+		this.pi = pi;
+	}
+
+	public double getPi() {
+		return pi;
+	}
+	
+	
+}
