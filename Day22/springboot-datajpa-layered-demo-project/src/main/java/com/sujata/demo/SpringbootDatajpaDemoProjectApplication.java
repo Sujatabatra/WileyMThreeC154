4fe50f2ec39bcd260e9c200e2c991476ba40c0e9
Sujatabatra <sujatabatra@hotmail.com>
2021-10-21 15:39:47 +0530

    Add files via upload

---
+++ Day22/springboot-datajpa-layered-demo-project/src/main/java/com/sujata/demo/SpringbootDatajpaDemoProjectApplication.java
@@ -0,0 +1,41 @@
+package com.sujata.demo;
+
+import java.util.Scanner;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.sujata.bean.Employee;
+import com.sujata.persistence.EmployeeDao;
+import com.sujata.presentation.EmployeePresentation;
+
+@SpringBootApplication(scanBasePackages = "com.sujata")
+@EnableJpaRepositories(basePackages = "com.sujata.persistence")
+@EntityScan(basePackages = "com.sujata.bean")
+public class SpringbootDatajpaDemoProjectApplication implements CommandLineRunner{
+
+	@Autowired
+	private EmployeePresentation employeePresentation;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDatajpaDemoProjectApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Scanner scanner=new Scanner(System.in);
+		while(true) {
+			employeePresentation.showMenu();
+			System.out.println("Enter Choice : ");
+			int choice=scanner.nextInt();
+			employeePresentation.performMenu(choice);
+			
+		}	
+		
+	}
+
+}
