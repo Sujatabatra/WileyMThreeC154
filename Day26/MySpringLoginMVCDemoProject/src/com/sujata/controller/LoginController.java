8f50697d86458f38236f6471ca6da36d3aad8a74
Sujatabatra <sujatabatra@hotmail.com>
2021-10-27 10:57:40 +0530

    Add files via upload

---
+++ Day26/MySpringLoginMVCDemoProject/src/com/sujata/controller/LoginController.java
@@ -0,0 +1,37 @@
+package com.sujata.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.sujata.bean.User;
+import com.sujata.model.service.UserService;
+
+@Controller
+public class LoginController {
+
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping("/")
+	public ModelAndView getMainPage() {
+		return new ModelAndView("login");
+	}
+	
+	@RequestMapping("/login")
+	public ModelAndView loginCheckController(HttpServletRequest request) {
+		String una=request.getParameter("uname");
+		String pwd=request.getParameter("pwd");
+		User user=new User(una,pwd);
+		String message=null;
+		if(userService.loginCheck(user))
+			message="Login Success!";
+		else
+			message="Login Failed!";
+		
+		return new ModelAndView("output", "msg", message);
+	}
+}
