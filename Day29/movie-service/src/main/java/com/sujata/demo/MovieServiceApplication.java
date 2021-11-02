61236d1a9b90efb3847af190d5fe0ce9b07f3c5a
Sujatabatra <sujatabatra@hotmail.com>
2021-11-02 12:03:42 +0530

    Add files via upload

---
+++ Day29/movie-service/src/main/java/com/sujata/demo/MovieServiceApplication.java
@@ -0,0 +1,33 @@
+package com.sujata.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.sujata.bean.Movie;
+import com.sujata.persistence.MovieDao;
+
+@SpringBootApplication(scanBasePackages = "com.sujata")
+@EntityScan(basePackages = "com.sujata.bean")
+@EnableJpaRepositories(basePackages = "com.sujata.persistence")
+public class MovieServiceApplication /*implements CommandLineRunner*/ {
+
+//	@Autowired
+//	private MovieDao movieDao;
+	public static void main(String[] args) {
+		SpringApplication.run(MovieServiceApplication.class, args);
+	}
+//	@Override
+//	public void run(String... args) throws Exception {
+//		movieDao.save(new Movie("M001","Bahubali"));
+//		movieDao.save(new Movie("M002", "RRR"));
+//		movieDao.save(new Movie("M003", "Titanic"));
+//		movieDao.save(new Movie("M004", "Tenant"));
+//		movieDao.save(new Movie("M005", "Inception"));
+//		
+//	}
+
+}
